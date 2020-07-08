148126c93c907897324b78da73bd338dfa78de3d
ingridnkenli <i.nkenlifack@gmail.com>
2020-07-08 08:18:49 -0400

    Added sign-in, sign-up buttons. Font, image, text images.

--- app/src/main/java/com/example/androideatit/MainActivity.java
+++ app/src/main/java/com/example/androideatit/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.androideatit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonSignIn, buttonSignUp;
+    TextView textSlogan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonSignIn = (Button) findViewById(R.id.buttonSignIn);
+        buttonSignUp = (Button) findViewById(R.id.buttonSignUp);
+        textSlogan = (TextView) findViewById(R.id.textSlogan);
+        Typeface face = Typeface.createFromAsset(getAssets(), "fonts/NABILA.rar");
+
+        textSlogan.setTypeface(face);
+        buttonSignIn.setOnClickListener();
+
     }
 }
